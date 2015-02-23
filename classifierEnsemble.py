556b772590173e05b3ffc7e2346875c981d9da86
Dylan Schultz <dylanschultz@beavernet-164.caltech.edu>
2015-02-22 16:28:18 -0800

    Trying tf-idf

--- classifierEnsemble.py
+++ classifierEnsemble.py
@@ -1,16 +1,32 @@
 import numpy as np
 from sklearn.cross_validation import cross_val_score
-from sklearn.cross_validation import train_test_split
-from sklearn.datasets import load_iris
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import ExtraTreesClassifier
-import matplotlib.pyplot as plt
 from sklearn.grid_search import GridSearchCV
-import pandas as pd
-from sklearn.cross_validation import KFold
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
 from sklearn.grid_search import GridSearchCV
-import csv
+
+# Import the word count data for training.
+data = np.genfromtxt('./data/kaggle_train_wc.csv', delimiter = ',')
+(row, col) = data.shape
+# format data
+xwc, ywc = data[:, :col-1], data[:, col-1]
+
+# Import the word count data for training.
+data = np.genfromtxt('./data/kaggle_train_tf_idf.csv', delimiter = ',')
+(row, col) = data.shape
+# format data
+xtf, ytf = data[:, :col-1], data[:, col-1]
+
+# Import the test data
+xtest_wc = np.genfromtxt('./data/kaggle_test_wc.csv', delimiter = ',')
+xtest_tf = np.genfromtxt('./data/kaggle_test_tf_idf.csv', delimiter = ',')
+
+# Train a MultinomialNB Model
+mnb = MultinomialNB()
+mnb_score_wc = cross_val_score(mnb, xwc, ywc)
+mnb_score_tf = cross_val_score(mnb, xtf, ytf)
+print('MNB WC: ', mnb_score_wc.mean())
+print('MNB TF-IDF: ', mnb_score_tf.mean())
